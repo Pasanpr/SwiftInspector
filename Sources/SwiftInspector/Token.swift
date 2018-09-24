c455b0d9c0992741329371084273d766c8f28031
Pasan Premaratne <pasan@premaratne.me>
2018-09-24 10:28:12 -0400

    Cleaned up Punctuation type

--- Sources/SwiftInspector/Token.swift
+++ Sources/SwiftInspector/Token.swift
@@ -12,10 +12,10 @@ import Foundation
 public enum Punctuation: String,  AutoEquatable {
     case leftParen = "\u{0028}"
     case rightParen = "\u{0029}"
-    case leftSquareBracket = "\u{005B}"
-    case rightSquareBracket = "\u{005D}"
     case leftCurlyBracket = "\u{007B}"
     case rightCurlyBracket = "\u{007D}"
+    case leftSquareBracket = "\u{005B}"
+    case rightSquareBracket = "\u{005D}"
     case dot = "\u{002E}"
     case comma = "\u{002C}"
     case colon = "\u{003A}"
