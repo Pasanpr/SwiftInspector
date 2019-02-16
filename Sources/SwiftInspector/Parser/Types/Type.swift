7bc6551a4ccd943fd036b257df5e3a9b1b99f110
Pasan Premaratne <pasan@premaratne.me>
2019-02-15 23:42:58 -0500

    Match type declarations

--- Sources/SwiftInspector/Parser/Types/Type.swift
+++ Sources/SwiftInspector/Parser/Types/Type.swift
@@ -13,6 +13,25 @@ public indirect enum Type {
     case function
     case typeIdentifier(identifier: String)
     case tuple
+    
+    func matchesType<T>(_ type: T.Type) -> Bool {
+        let typeDescription = String(describing: type)
+        return self.description == typeDescription
+    }
 }
 
 extension Type: AutoEquatable {}
+
+extension Type: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .array(let type):
+            return "Array<\(String(describing: type))>"
+        case .dictionary(let keyType, let valueType):
+            return "Dictionary<\(String(describing: keyType)), \(String(describing: valueType))>"
+        case .function: fatalError()
+        case .typeIdentifier(let identifier): return identifier
+        case .tuple: fatalError()
+        }
+    }
+}
